636439ee86f81a49ba2ec15370b7c1c848c6b19d
Stephen Marsh <stephenhmarsh@users.noreply.github.com>
2022-08-11 21:47:00 -0400

    improving the hacky proof of concept, but getting extra pinch event and a race condition

--- src/components/SVGViewboxZoom/SVGViewboxZoom.jsx
+++ src/components/SVGViewboxZoom/SVGViewboxZoom.jsx
@@ -1,40 +1,70 @@
-import React, { useRef, useState } from "react";
-import { usePinch } from '@use-gesture/react'
-
-// for more efficient tree-shaking than direct import
-// const useGesture = createUseGesture([dragAction, pinchAction])
+import React, { useRef, useState, useEffect } from "react";
+import { useGesture, usePinch } from '@use-gesture/react'
 
 const SVGViewboxZoom = ({ SVG, initivalViewboxValues = [0, 0, 0, 0] }) => {
 	const [viewboxValues, setViewboxValues] = useState(initivalViewboxValues)
+	const [lastSetViewboxValues, setLastSetViewboxValues] = useState(viewboxValues);
+	const [lastRunTimeStamp, setLastRunTimeStamp] = useState(0);
+
 	const target = useRef();
 
-	const handlePinch = (e) => {
-		// console.log("handlePinch", e.origin[0]);
-		// const sign = e.direction[0];
+	useEffect(() => {
+		document.addEventListener('gesturestart', (e) => e.preventDefault())
+		document.addEventListener('gesturechange', (e) => e.preventDefault())
+	})
+
+	const handlePinch = (state) => {
+		console.log(state.last, state.timeStamp, lastRunTimeStamp)
+		console.log("handlePinch", state);
+
+
+		if (state.first && state.timeStamp < (lastRunTimeStamp + 500)) {
+			console.log("should cancel")
+			state.cancel()
+			return;
+		}
+
+		const scaleFactor = state.movement[0]
 		const results = [
-			(viewboxValues[0] * 1),
-			(viewboxValues[1] * 1),
-			Math.min(viewboxValues[2] / e.da[0], initivalViewboxValues[2]),
-			Math.min(viewboxValues[3] / e.da[0], initivalViewboxValues[3])
+			0,
+			0,
+			lastSetViewboxValues[2] / scaleFactor,
+			lastSetViewboxValues[3] / scaleFactor
 		]
 
-		console.log("setting", results[2], results[3])
-		setViewboxValues(
-			results
-		)
+		if (state.pinching) {
+			// console.log("setting", results[2], results[3])
+			setViewboxValues(results)
+		}
+		if (state.last) {
+			setLastSetViewboxValues(results)
+			setLastRunTimeStamp(state.timeStamp)
+		}
+		// }
 	}
 
-	usePinch(
-		handlePinch,
-		{
-			// "Because React doesn't support proprietary Webkit GestureEvents, you will need to attach the gesture using a ref, with the target option."
-			// https://use-gesture.netlify.app/docs/gestures/#about-the-pinch-gesture
-			// https://use-gesture.netlify.app/docs/options/#target
-			target
-		}
-	)
+	// const handlePinchEnd = (state) => {
+	// 	console.log("handlePinchEnd", state);
 
-	return <div style={{ width: "100%", border: "1px solid red" }} ref={target}>< SVG viewBox={viewboxValues.join(" ")} /></div>
+	// }
+
+	usePinch(handlePinch, {
+		// "Because React doesn't support proprietary Webkit GestureEvents, you will need to attach the gesture using a ref, with the target option."
+		// https://use-gesture.netlify.app/docs/gestures/#about-the-pinch-gesture
+		// https://use-gesture.netlify.app/docs/options/#target
+		target,
+		// just to be sure elsaticity doesn't fire weird values/extra first/lasts
+		rubberband: false,
+	})
+
+	// const bind = useGesture(
+	// 	{ onPinchEnd: handlePinchEnd },
+	// 	{ pinch: { target } }
+	// )
+
+	return <div style={{ width: "100%", border: "1px solid red" }} ref={target} >
+		<SVG viewBox={viewboxValues.join(" ")} />
+	</div>
 }
 
 export default SVGViewboxZoom
